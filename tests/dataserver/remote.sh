adc4a2d1444c7c462235dd9132d0414d768da51d
Martynas Bagdonas <martbgd@gmail.com>
2017-04-30 08:52:02 +0300

    .

--- tests/dataserver/remote.sh
+++ tests/dataserver/remote.sh
@@ -3,5 +3,5 @@
 echo "Running dataserver remote tests"
 docker-compose exec dataserver bash -c " \
 	cd /var/www/dataserver/tests/remote \
-	&& ./phpunit --debug tests/API/ --exclude-group s3,sns,attachment,api-sync
+	&& ./phpunit --debug tests/API/ --exclude-group s3,sns,attachments,classic-sync,failing-translation
 	"
